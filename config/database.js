a8999ae2e0a0dd4caaa6172dafc9efc0a82fbe61
singjun <shinsj4653@gmail.com>
2023-01-13 14:33:31 +0900

    modified pacakge.json : npm start -> initiate nodemon index.js & added db info

--- config/database.js
+++ config/database.js
@@ -3,11 +3,11 @@ const {logger} = require('./winston');
 
 // TODO: 본인의 DB 계정 입력
 const pool = mysql.createPool({
-    host: '',
-    user: '',
+    host: 'http://database-1.coiwdqamvdyz.ap-northeast-2.rds.amazonaws.com/',
+    user: 'admin',
     port: '3306',
-    password: '',
-    database: ''
+    password: '10131013',
+    database: 'Potshe'
 });
 
 module.exports = {
